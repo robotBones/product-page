be342e7ab87d23f063d637eded575fbfeae3c391
Kim Marquardt <kim.marquardt@gmail.com>
2017-11-27 20:39:01 -0800

    Implement product details and cart

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+// accordion logic
 var accordion = document.getElementsByClassName("accordion");
 for (var i = 0; i < accordion.length; i++) {
   accordion[i].onclick = function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+    this.classList.toggle("js-active");
 
+    var icon = this.previousElementSibling;
+    icon.classList.toggle('js-collapsed');
+    icon.classList.toggle('js-expanded');
+
+    var panel = this.nextElementSibling;
     if (panel.style.maxHeight){
       panel.style.maxHeight = null;
     } else {
@@ -11,3 +16,24 @@ for (var i = 0; i < accordion.length; i++) {
     }
   }
 }
+
+function message(msg) {
+  const modal = document.getElementsByClassName("js-modal")[0];
+  const message = document.getElementsByClassName("js-modal-message")[0];
+  modal.classList.remove('js-hidden');
+  message.innerText = msg;
+
+  setTimeout(function() {
+    modal.classList.add('js-hidden');
+  }, 3000);
+}
+
+// form logic
+var form = document.getElementsByClassName("cta-cart")[0];
+form.onsubmit = function(event) {
+  event.preventDefault();
+  const quantity = document.getElementsByClassName("quantity")[0].value
+  const msg = "You've added " + quantity + " apron(s)!";
+  message(msg);
+}
+
